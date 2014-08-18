c49153858d0b682f7d0f9c3a82546dc4505c240e
pcm <pcm@pcm.(none)>
2014-08-17 23:33:24 -0700

    adding event page

--- www/pages/basic/js/oplz.page.basic.js
+++ www/pages/basic/js/oplz.page.basic.js
@@ -8,40 +8,62 @@ $(function(){
 			"js": "pages/splash/js",
 			"css": "pages/splash/style/css"
 		},
-		"render": function(data){
+		"templateData": {
+		},
+		"pageData": {
+			"defaults": {}
+		},
+		"render": function (data){
 			var page = this;
 
 			if (!page.template){
-				$.get( page.paths.templates + "/oplz.splash.tmpl").done(function(tmpl){
+				/**
+				 * @todo Change uri to dynamically select template based on screen.
+				 */
+				$.get( page.paths.templates + "/oplz.myschedule."+window.oplz.common.screen.current+".tmpl").done(function (tmpl){
+					page.templateString = tmpl;
 					page.template = window.oplz.common.pages.build(page, tmpl);
 
 					window.oplz.common.pages.embed( page );
 
-					page.bind();
-
 					window.oplz.common.pages.display( page );
+
+					page.getData(function (data){
+						page.bind();
+
+						page.scheduleRenderData();
+
+						page.miniMenuRenderData();
+
+						page.recentSpeakersRenderData();
+					});
+
 					window.oplz.common.route.loaded(); // todo: turn into a call to an event listener for loaded
 				});
 			} else {
 				window.oplz.common.pages.display( page );
 			}
 		},
-		"adjustToDevice": function(){
-			switch (window.oplz.common.screen.detect()){
-				case window.oplz.common.screen.size.desktop:
-				case window.oplz.common.screen.size.bigTable:
-				case window.oplz.common.screen.size.smallTablet:
-				case window.oplz.common.screen.size.bigPhone:
-				case window.oplz.common.screen.size.smallPhone:
 
-			}
-			if (window.oplz.common.screen.detect("bigPhone", ">")){
-			}
-		},
-		"bind": function(){
-			$("#page-container-splash").delegate(".oplz-splash-option-container", "click", function(){
-				location.hash = "#!" + $(this).data("gotopage");
+		"getData": function(callback){
+			var _this = this;
+			cached = cached !== undefined ? cached : true;
+
+			oplz.common.ajax.tools.group({
+				"getEvents": [{}, function(data){
+					window.oplz.common.events.load(data.data);
+				}, {}, cached ],
+				"getUsers": [{}, function(data){
+					window.oplz.common.users.load(data.data)
+					window.oplz.me = window.oplz.common.users.get( window.oplz.users.usernames["tamara14"] );
+				}, {}, cached ]
+			}, {
+				"finished": callback
 			});
+		},
+
+		"bind": function(){
+			// $("#page-container-splash").delegate("", "click", function(){});
 		}
 	});
 
